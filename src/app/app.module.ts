79b35572bd63bee67657632e402c1b8598acf643
Payaam Emami <payaamemami@gmail.com>
2019-05-31 15:08:19 -0700

    added routing and about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,25 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginTwitchComponent } from './components/login-twitch/login-twitch.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomePromoComponent } from './components/home/home-promo/home-promo.component';
 import { HomeHeaderComponent } from './components/home/home-header/home-header.component';
+import { AboutComponent } from './components/about/about.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
@@ -26,17 +35,20 @@ import { HomeHeaderComponent } from './components/home/home-header/home-header.c
     FooterComponent,
     HomePromoComponent,
     HomeHeaderComponent,
+    AboutComponent,
+    PageHeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    LayoutModule,
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
     MatToolbarModule,
-    LayoutModule
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
