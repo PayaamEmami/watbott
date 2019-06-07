be3eab92aa9d53995f502bcd41a11096cbb9ccfe
Payaam Emami <payaamemami@gmail.com>
2019-06-06 22:33:27 -0700

    started server.js and added packages

---
+++ src/server/server.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const OAuth2Strategy = require("passport-oauth").OAuth2Strategy;
+const request = require("request");
+const app = express();
+const port = 3000;
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+OAuth2Strategy.prototype.userProfile = (accessToken, done) => {
+  var options = {
+    url: "https://api.twitch.tv/helix/users",
+    method: "GET",
+    headers: {
+      "Client-ID": process.env.TWITCH_CLIENT_ID,
+      Accept: "application/vnd.twitchtv.v5+json",
+      Authorization: "Bearer " + accessToken
+    }
+  };
+
+  request(options, (error, response, body) => {
+    if (response && response.statusCode == 200) {
+      done(null, JSON.parse(body));
+    } else {
+      done(JSON.parse(body));
+    }
+  });
+};
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  "twitch",
+  new OAuth2Strategy(
+    {
+      authorizationUrl: "https://id.twitch.tv/oauth2/authorize",
+      tokenUrl: "https://id.twitch.tv/oauth2/token",
+      clientID: process.env.TWITCH_CLIENT_ID,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+      state: true
+    },
+    (accessToken, refreshToken, profile, done) => {
+      profile.accessToken = accessToken;
+      profile.refreshToken = refreshToken;
+
+      done(null, profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/twitch",
+  passport.authenticate("twitch", { scope: "user_read" })
+);
+
+app.get(
+  "/auth/twitch/callback",
+  passport.authenticate("twitch", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+app.get("/", (req, res) => {
+  if (req.session && req.session.passport && req.session.passport.user) {
+    res.send("dashboard");
+  } else {
+    res.send("root");
+  }
+});
+
+app.listen(port, () => console.log("Server started on port " + port));
