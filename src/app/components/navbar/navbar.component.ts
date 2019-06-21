3d9fd682b81e14abbe3eb5195f345f9e5ea0871d
Payaam Emami <payaamemami@gmail.com>
2019-06-21 03:24:30 -0700

    Cleaned up navbar component after moving socials into its own component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material/icon';
 import { AuthenticationService, Auth } from '../../services/authentication.service';
 import { UserService, User } from '../../services/user.service';
 
@@ -10,23 +8,14 @@ import { UserService, User } from '../../services/user.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  twitterUrl = 'https://twitter.com/WatBott';
-  githubUrl = 'https://github.com/PayaamEmami/watbott';
   logoutUrl = 'http://localhost:3000/user/logout';
-  githubImage = '../../assets/img/socials/github_white.png';
   watbottImage = '../../assets/img/logo/watbott_background_icon.png';
   isAuthenticated = false;
   username = '';
   userImage = '';
 
   constructor(private authService: AuthenticationService,
-              private userService: UserService,
-              iconRegistry: MatIconRegistry,
-              sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-      'twitter-logo',
-      sanitizer.bypassSecurityTrustResourceUrl('../../assets/img/socials/twitter_white.svg')
-    );
+              private userService: UserService) {
 
     this.authService.isAuthenticated().subscribe((data: Auth) => {
       this.isAuthenticated = (data.auth === 'true') ? true : false;
