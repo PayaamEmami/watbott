0b258d563b0cdb815f573da1cfb6321596fe9384
Payaam Emami <payaamemami@gmail.com>
2019-06-30 19:16:24 -0700

    added connect/disconnect functions to module exports

--- src/server/bot/bot.js
+++ src/server/bot/bot.js
@@ -4,24 +4,52 @@ const process = require("process");
 const tmi = require("tmi.js");
 const watson = require("./watson");
 
-const opts = {
+let opts = {
   identity: {
     username: process.env.TWITCH_BOT_USERNAME,
     password: process.env.TWITCH_OAUTH_TOKEN
   },
-  channels: [process.env.TWITCH_CHANNEL_USERNAME]
+  channels: []
 };
-const twitchClient = new tmi.client(opts);
-let session_id;
+let twitchClient = new tmi.client(opts);
+let sessionId;
 
-twitchClient.connect();
+module.exports.connect = async channelName => {
+  try {
+    this.opts.channels = this.opts.channels.push(channelName);
+    twitchClient.connect();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports.disconnect = async channelName => {
+  try {
+    let tempOpts = {
+      identity: {
+        username: process.env.TWITCH_BOT_USERNAME,
+        password: process.env.TWITCH_OAUTH_TOKEN
+      },
+      channels: [channelName]
+    };
+    let tempClient = new tmi.client(tempOpts);
+
+    tempClient.disconnect();
+
+    watson.deleteSession(sessionId);
+
+    this.opts.channels.splice(this.opts.channels.indexOf(channelName), 1);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 twitchClient.on("connected", (address, port) => {
   console.log(`* Connected to ${address}:${port}`);
   watson
     .createSession()
     .then(res => {
-      session_id = res.session_id;
+      sessionId = res.session_id;
     })
     .catch(err => {
       console.error(err);
@@ -35,7 +63,7 @@ twitchClient.on("message", (channel, userstate, message, self) => {
 
   if (message.toLowerCase().includes(process.env.TWITCH_BOT_USERNAME)) {
     watson
-      .message(message, session_id)
+      .message(message, sessionId)
       .then(res => {
         let response = res.output.generic[0].text;
         let intent = res.output.intents[0].intent;
@@ -59,7 +87,7 @@ twitchClient.on("message", (channel, userstate, message, self) => {
 twitchClient.on("disconnected", reason => {
   console.log(`* Disconnected: ${reason}`);
   watson
-    .deleteSession(session_id)
+    .deleteSession(sessionId)
     .then(res => {
       console.log("* Watson session deleted.");
     })
