ec0eef8215573dd6d4fb70e53e44aeb3d388be6a
Payaam Emami <payaamemami@gmail.com>
2019-06-30 19:18:07 -0700

    prettier format

--- src/server/bot/twitch.js
+++ src/server/bot/twitch.js
@@ -1,17 +1,20 @@
-require('dotenv').config();
-const fetch = require('node-fetch');
+require("dotenv").config();
+const fetch = require("node-fetch");
 
 module.exports.getStream = async () => {
   try {
     return await fetch(
-      `https://api.twitch.tv/helix/streams?user_login=${process.env.TWITCH_CHANNEL_USERNAME}`,
+      `https://api.twitch.tv/helix/streams?user_login=${
+        process.env.TWITCH_CHANNEL_USERNAME
+      }`,
       {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'Client-ID': process.env.TWITCH_CLIENT_ID
+          "Client-ID": process.env.TWITCH_CLIENT_ID
         }
-      });
+      }
+    );
   } catch (err) {
     console.error(err);
   }
-}
+};
--- src/server/bot/watson.js
+++ src/server/bot/watson.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const AssistantV2 = require('ibm-watson/assistant/v2');
+require("dotenv").config();
+const AssistantV2 = require("ibm-watson/assistant/v2");
 
 const service = new AssistantV2({
   iam_apikey: process.env.WATSON_API_KEY,
@@ -15,7 +15,7 @@ module.exports.createSession = async () => {
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 module.exports.message = async (msg, session_id) => {
   msg = filterMessage(msg);
@@ -25,16 +25,16 @@ module.exports.message = async (msg, session_id) => {
       assistant_id: process.env.WATSON_ASSISTANT_ID,
       session_id: session_id,
       input: {
-        'message_type': 'text',
-        'text': msg
+        message_type: "text",
+        text: msg
       }
     });
   } catch (err) {
     console.error(err);
   }
-}
+};
 
-module.exports.deleteSession = async (session_id) => {
+module.exports.deleteSession = async session_id => {
   try {
     return await service.deleteSession({
       assistant_id: process.env.WATSON_ASSISTANT_ID,
@@ -43,8 +43,8 @@ module.exports.deleteSession = async (session_id) => {
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 function filterMessage(message) {
-  return message.replace(`@*${process.env.TWITCH_BOT_USERNAME}`, '').trim();
+  return message.replace(`@*${process.env.TWITCH_BOT_USERNAME}`, "").trim();
 }
