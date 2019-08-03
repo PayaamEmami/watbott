4e7baabd5dde9e6672df35e766d3dcd71f577087
Payaam Emami <contact@payaame.com>
2019-08-03 15:16:35 -0700

    refactored authservice tests

--- src/client/services/auth.service.spec.ts
+++ src/client/services/auth.service.spec.ts
@@ -12,79 +12,83 @@ describe('AuthService', () => {
     authService = new AuthService(httpClientSpy as any);
   });
 
-  it('getAuth should only call HttpClient once', () => {
-    const expectedAuth: Auth[] = [{ isAuthenticated: true }];
+  describe('getAuth', () => {
+    it('should only call HttpClient once', () => {
+      const expectedAuth: Auth[] = [{ isAuthenticated: true }];
 
-    httpClientSpy.get.and.returnValue(asyncData(expectedAuth));
+      httpClientSpy.get.and.returnValue(asyncData(expectedAuth));
 
-    authService.getAuth().subscribe(
-      () => { },
-      fail
-    );
+      authService.getAuth().subscribe(
+        () => { },
+        fail
+      );
 
-    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
-  });
-
-  it('getAuth should return expected Auth', async () => {
-    const expectedAuth: Auth[] = [{ isAuthenticated: true }];
-
-    httpClientSpy.get.and.returnValue(asyncData(expectedAuth));
-
-    await authService.getAuth().subscribe(
-      auth => expect(auth).toEqual(expectedAuth, 'expected Auth'),
-      fail
-    );
-  });
-
-  it('getAuth should return an error when the server returns a 404', async () => {
-    const errorResponse = new HttpErrorResponse({
-      error: 'test 404 error',
-      status: 404, statusText: 'Not Found'
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
     });
 
-    httpClientSpy.get.and.returnValue(asyncError(errorResponse));
+    it('should return expected Auth', async () => {
+      const expectedAuth: Auth[] = [{ isAuthenticated: true }];
 
-    await authService.getAuth().subscribe(
-      auth => fail('expected an error, not auth'),
-      error => expect(error).toEqual('Something bad happened; please try again later.')
-    );
-  });
+      httpClientSpy.get.and.returnValue(asyncData(expectedAuth));
 
-  it('logout should only call HttpClient once', () => {
-    const expectedAuth: Auth[] = [{ isAuthenticated: true }];
-
-    httpClientSpy.put.and.returnValue(asyncData(expectedAuth));
-
-    authService.logout().subscribe(
-      () => { },
-      fail
-    );
-
-    expect(httpClientSpy.put.calls.count()).toBe(1, 'one call');
-  });
-
-  it('logout should return expected Auth', async () => {
-    const expectedAuth: Auth[] = [{ isAuthenticated: false }];
-
-    httpClientSpy.put.and.returnValue(asyncData(expectedAuth));
-
-    await authService.logout().subscribe(
-      auth => expect(auth).toEqual(expectedAuth, 'expected Auth'),
-      fail
-    );
-  });
-
-  it('logout should return an error when the server returns a 404', async () => {
-    const errorResponse = new HttpErrorResponse({
-      error: 'test 404 error',
-      status: 404, statusText: 'Not Found'
+      await authService.getAuth().subscribe(
+        auth => expect(auth).toEqual(expectedAuth, 'expected Auth'),
+        fail
+      );
     });
 
-    httpClientSpy.put.and.returnValue(asyncError(errorResponse));
+    it('should return an error when the server returns a 404', async () => {
+      const errorResponse = new HttpErrorResponse({
+        error: 'test 404 error',
+        status: 404, statusText: 'Not Found'
+      });
 
-    await authService.logout().subscribe(
-      auth => fail('expected an error, not auth'),
-      error => expect(error).toEqual('Something bad happened; please try again later.')
-    );
+      httpClientSpy.get.and.returnValue(asyncError(errorResponse));
+
+      await authService.getAuth().subscribe(
+        auth => fail('expected an error, not auth'),
+        error => expect(error).toEqual('Something bad happened; please try again later.')
+      );
+    });
+  });
+
+  describe('logout', () => {
+    it('should only call HttpClient once', () => {
+      const expectedAuth: Auth[] = [{ isAuthenticated: true }];
+
+      httpClientSpy.put.and.returnValue(asyncData(expectedAuth));
+
+      authService.logout().subscribe(
+        () => { },
+        fail
+      );
+
+      expect(httpClientSpy.put.calls.count()).toBe(1, 'one call');
+    });
+
+    it('should return expected Auth', async () => {
+      const expectedAuth: Auth[] = [{ isAuthenticated: false }];
+
+      httpClientSpy.put.and.returnValue(asyncData(expectedAuth));
+
+      await authService.logout().subscribe(
+        auth => expect(auth).toEqual(expectedAuth, 'expected Auth'),
+        fail
+      );
+    });
+
+    it('should return an error when the server returns a 404', async () => {
+      const errorResponse = new HttpErrorResponse({
+        error: 'test 404 error',
+        status: 404, statusText: 'Not Found'
+      });
+
+      httpClientSpy.put.and.returnValue(asyncError(errorResponse));
+
+      await authService.logout().subscribe(
+        auth => fail('expected an error, not auth'),
+        error => expect(error).toEqual('Something bad happened; please try again later.')
+      );
+    });
   });
 });
