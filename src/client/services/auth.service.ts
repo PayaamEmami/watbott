610e6b638b612b31df2450da08e72cdc393e3a6b
Payaam Emami <contact@payaame.com>
2019-08-03 12:30:41 -0700

    added base service to handle error

--- src/client/services/auth.service.ts
+++ src/client/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
+import { BaseService } from './base.service';
 
 export interface Auth {
   isAuthenticated: boolean;
@@ -10,21 +12,9 @@ export interface Auth {
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService extends BaseService {
 
-  constructor(private http: HttpClient) { }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.error('An error occurred:', error.error.message);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    return throwError(
-      'Something bad happened; please try again later.');
-  }
+  constructor(private http: HttpClient) { super(); }
 
   getAuth(): Observable<any> {
     return this.http.get<Auth>('/auth',
--- src/client/services/user.service.ts
+++ src/client/services/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
+import { BaseService } from './base.service';
 
 export interface User {
   login: string;
@@ -12,23 +14,11 @@ export interface User {
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService extends BaseService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { super(); }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.error('An error occurred:', error.error.message);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    return throwError(
-      'Something bad happened; please try again later.');
-  }
-
-  getUser() {
+  getUser(): Observable<any> {
     return this.http.get<User>('/api/user',
       { withCredentials: true, responseType: 'json' })
       .pipe(
