7434f75f22aa14e6d10c8518f12c65a86858f4ea
Payaam Emami <contact@payaame.com>
2019-08-05 13:53:16 -0700

    empty base service tests & added user service tests

--- src/client/services/user.service.spec.ts
+++ src/client/services/user.service.spec.ts
@@ -1,12 +1,54 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpErrorResponse } from '@angular/common/http';
 
-import { UserService } from './user.service';
+import { UserService, User } from './user.service';
+import { asyncData, asyncError } from '../../test/async-observable-helpers';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpClientSpy: { get: jasmine.Spy };
+  let userService: UserService;
 
-  it('should be created', () => {
-    const service: UserService = TestBed.get(UserService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    userService = new UserService(httpClientSpy as any);
+  });
+
+  describe('getUser', () => {
+    it('should only call HttpClient once', () => {
+      const expectedUser: User[] = [{ login: 'testLogin', profileImage: 'testProfileImage', isWhitelisted: true }];
+
+      httpClientSpy.get.and.returnValue(asyncData(expectedUser));
+
+      userService.getUser().subscribe(
+        () => { },
+        fail
+      );
+
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+
+    it('should return expected User', async () => {
+      const expectedUser: User[] = [{ login: 'testLogin', profileImage: 'testProfileImage', isWhitelisted: true }];
+
+      httpClientSpy.get.and.returnValue(asyncData(expectedUser));
+
+      await userService.getUser().subscribe(
+        user => expect(user).toEqual(expectedUser, 'expected User'),
+        fail
+      );
+    });
+
+    it('should return an error when the server returns a 404', async () => {
+      const errorResponse = new HttpErrorResponse({
+        error: 'test 404 error',
+        status: 404, statusText: 'Not Found'
+      });
+
+      httpClientSpy.get.and.returnValue(asyncError(errorResponse));
+
+      await userService.getUser().subscribe(
+        () => fail('expected an error, not user'),
+        error => expect(error).toEqual('Something bad happened; please try again later.')
+      );
+    });
   });
 });
