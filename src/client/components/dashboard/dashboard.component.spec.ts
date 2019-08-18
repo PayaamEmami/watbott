d9cb34aca2d8b2ed903e07ca3eef6407102e0b3f
Payaam Emami <contact@payaame.com>
2019-08-18 11:57:25 -0700

    added test for dashboard component

--- src/client/components/dashboard/dashboard.component.spec.ts
+++ src/client/components/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,67 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 import { DashboardComponent } from './dashboard.component';
+import { DashboardDialogComponent } from './dashboard-dialog/dashboard-dialog.component';
+import { UserService } from 'src/client/services/user.service';
+import { AuthService } from 'src/client/services/auth.service';
+
+@Component({ selector: 'app-page-header', template: '' })
+class PageHeaderComponent { }
+
+@Component({ selector: 'app-dashboard-content', template: '' })
+class DashboardContentComponent { }
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
-  }));
+  let userServiceSpy: { getUser: jasmine.Spy };
+  let authServiceSpy: { getAuth: jasmine.Spy, logout: jasmine.Spy };
+  let dialogSpy: { open: jasmine.Spy };
 
   beforeEach(() => {
+    userServiceSpy = jasmine.createSpyObj('UserService', ['getUser']);
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['getAuth', 'logout']);
+    dialogSpy = jasmine.createSpyObj('Dialog', ['open']);
+
+    TestBed.configureTestingModule({
+      declarations: [
+        DashboardComponent,
+        DashboardContentComponent,
+        DashboardDialogComponent,
+        PageHeaderComponent,
+      ],
+      imports: [
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: UserService, useValue: userServiceSpy },
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: MatDialog, useValue: dialogSpy }
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call open dialog once if user is not whitelisted', () => {
+    userServiceSpy.getUser.and.returnValue(
+      of({
+        login: 'testLogin',
+        profileImage: 'testProfileImage',
+        isWhitelisted: false
+      })
+    );
+
+    fixture.detectChanges();
+
+    expect(dialogSpy.open.calls.count()).toBe(1, 'one call');
   });
 });
